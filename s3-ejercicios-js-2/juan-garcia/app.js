813d0b8801e74d75dda44d335112aad7672118e2
Juan Garcia <juanes19gh@gmail.com>
2026-02-25 09:58:21 -0500

    actualizacion

--- s3-ejercicios-js-2/juan-garcia/app.js
+++ s3-ejercicios-js-2/juan-garcia/app.js
@@ -1,13 +1,30 @@
-// Acceder a un elemento con id="mi-elemento"
+//POR ID
 let elemento = document.getElementById("mi-elemento");
+elemento.innerHTML = "Hola, mundo!";//modifica
 elemento.style.color = "Blue"; // Cambia el color del texto a rojo
-elemento.innerHTML = "Hola, mundo!";
 console.log(elemento); // Muestra el elemento en la consola
 
-// Cambiar el estilo de elementos con class="mi-clase"
+//Por CLASES
+
 var elementos = document.getElementsByClassName("mi-clase");
 for (var i = 0; i < elementos.length; i++) {
 	elementos[i].style.color = "green"; // Cambia el color del texto a rojo
 }
-
+for (var i = 0; i < elementos.length; i++) {
+	elementos[i].innerHTML = "Hola, mundo!"; //MODIFICA
+}
 console.log(elementos); // Muestra la colección de elementos en la consola
+
+//POR ETIQUETAS
+let parrafos = document.getElementsByTagName('p');
+
+for (let i = 0; i <parrafos.length; i++) {
+	parrafos[i].innerHTML = 'Hola mundo ' + (i + 1); //MODIFICAR
+}
+
+for (let i = 0; i < parrafos.length; i++) {
+    parrafos[i].style.backgroundColor = 'yellow'; //CAMBIO COLOR Y FUENTE
+	parrafos[i].style.fontWeight = 'bold';
+}
+
+console.log(parrafos); // Muestra una colección de todos los elementos
