312d52d6f6630768095fab9d19ad49d5c1442f20
Elkin Garces <e.garces@unimayor.edu.co>
2026-03-04 09:35:47 -0500

    Clase_4

---
+++ s3-ejercicio-s2/elkin-garces/app.js
@@ -0,0 +1,31 @@
+let elemento = document.getElementById("mi-elemento");
+console.log(elemento);
+
+elemento.innerHTML = "Hola Mundo!";
+elemento.style.color = "green";
+
+
+let elementos = document.getElementsByClassName("mi-clase");
+console.log(elementos);
+
+
+for (var i = 0; i < elementos.length; i++) {
+    elementos[i].innerHTML = "LOL xd ";
+}
+
+for (var i = 0; i < elementos.length; i++) {
+    elementos[i].style.color = "green";
+}
+
+
+let parrafos = document.getElementsByTagName('p');
+console.log(parrafos); 
+
+for (let i = 0; i <parrafos.length; i++) {
+    parrafos[i].innerHTML = 'Modificando parrafo modificado' + (i + 1);
+}
+
+for (let i = 0; i < parrafos.length; i++) {
+    parrafos[i].style.color = "blue";
+    parrafos[i].style.fontWeight = "bold";
+}
