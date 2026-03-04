fa9670f1b3c4dfd254ccea0658ef697eb0831d2b
Juan David Personal <juandare567@gmail.com>
2026-03-04 07:43:01 -0500

    cambionuevo

--- s3-ejercicios-js-2/Juan-Villamarin/app.js
+++ s3-ejercicios-js-2/Juan-Villamarin/app.js
@@ -7,7 +7,21 @@ console.log(elemento); // Muestra el elemento en la consola
 // Cambiar el estilo de elementos con class="mi-clase"
 var elementos = document.getElementsByClassName("mi-clase");
 for (var i = 0; i < elementos.length; i++) {
-	elementos[i].style.color = "green"; // Cambia el color del texto a rojo
+	elementos[i].style.color = "red"; // Cambia el color del texto a rojo
 }
 
 console.log(elementos); // Muestra la colección de elementos en la consola
+
+let parrafos = document.getElementsByTagName('p');
+console.log(parrafos); // Muestra una colección de todos los elementos
+
+let parrafos1 = document.getElementsByTagName('p');
+for (let i = 0; i <parrafos.length; i++) {
+	parrafos[i].innerHTML = 'Hola mundo ' + (i + 1);
+}
+
+for (let i = 0; i < parrafos.length; i++) {
+	parrafos[i].style.backgroundColor = 'yellow';
+	parrafos[i].style.fontWeight = 'bold';
+}
+
