580c597a3e9800afdb42c66fb5038b86f044af9b
Yeferson Gaviria <gaviriaceron001@gmail.com>
2026-03-16 10:35:26 -0500

    Avances semana 4

---
+++ s4-backend/yeferson-gaviria/app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+
+const bodyParser = require('body-parser');
+
+const cors = require('cors');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+
+
+
+const usr = {
+
+	name: 'María',
+	
+	age: '33',
+	
+	email: 'maria.gmail.com'
+
+};
+
+app.use(cors()); // Habilitar CORS para todas las rutas
+
+app.get('/user', (req, res) => {
+	res.json(usr); // envía el objeto literal al cliente
+});
+// TODO: CREAR AQUÍ LOS DEMÁS MÉTODOS
+app.listen(3000, () => {
+	console.log('El servidor está escuchando en el puerto 3000');
+});
+
