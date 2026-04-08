a679a14e8008782930f2c9f73cc291d575940c32
tezlayf <carlosgaviria@unimayor.edu.co>
2026-04-08 14:32:17 -0500

    Clean up script.js by removing comments
    Removed commented lines to clean up the code.

--- Ejercicios/responsive-demo/script.js
+++ Ejercicios/responsive-demo/script.js
@@ -1,5 +1,4 @@
-// listo para interacción futura
-// pequeño detalle para demostrar manejo JS
+
 
 document.addEventListener("DOMContentLoaded", () => {
   console.log("Interfaz responsive cargada correctamente");
