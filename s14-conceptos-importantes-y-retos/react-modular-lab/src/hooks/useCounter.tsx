55a4292b4c27c65b20fc8c47c299479dda88c67d
CRISTIAN AGREDO <C.AGREDO@UNIMAYOR.EDU.CO>
2026-05-20 09:08:48 -0500

    Custom hook

--- s14-conceptos-importantes-y-retos/react-modular-lab/src/hooks/useCounter.tsx
+++ s14-conceptos-importantes-y-retos/react-modular-lab/src/hooks/useCounter.tsx
@@ -1,15 +1,31 @@
-import { useState } from 'react'
-
-export const useCounter = () => {
-    const [count, setCount] = useState(10);
-    const increaseBy = (value: number) => {
-        setCount(count + value);
-    }; 
-    // 1 
-    return {
-        //properties
-        count,
-        //actions
-        increaseBy,
-    };
-};
+import { useCounter } from "../hooks/useCounter";
+export const Counter = () => {
+    // 1
+    const { count, increaseBy } = useCounter();
+    // 2
+    return (
+        <div>
+            <h3>
+                Counter
+								{/* 3 */}
+                <small>{" " + count}</small>
+            </h3>
+            <div className="flex gap-4">
+                <button
+                    className="p-2 bg-blue-500 rounded-xs w-10 text-white"
+                    // 3 
+                    onClick={() => increaseBy(+1)}
+                >
+                    +1
+                </button>
+                <button
+                    className="p-2 bg-blue-500 rounded-xs w-10 text-white"
+                    // 3
+                    onClick={() => increaseBy(-1)}
+                >
+                    -1
+                </button>
+            </div>
+        </div>
+    )
+}
