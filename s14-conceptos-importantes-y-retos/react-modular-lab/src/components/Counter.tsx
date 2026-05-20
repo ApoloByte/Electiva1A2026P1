04ed4a8eb98434af3c83e2b0ee13fec25d089876
Alex Vernaza <alexvernaza256@gmail.com>
2026-05-20 09:07:45 -0500

    Habilitar y Deshabilitar boton

--- s14-conceptos-importantes-y-retos/react-modular-lab/src/components/Counter.tsx
+++ s14-conceptos-importantes-y-retos/react-modular-lab/src/components/Counter.tsx
@@ -1,32 +1,28 @@
-import React from 'react'
-import { useCounter } from '../hooks/useCounter'
 
+import { useCounter } from "../hooks/useCounter";
+// 1.
+import { StyledButton } from "./StyledButton";
 export const Counter = () => {
-  const { count, increaseBy} = useCounter();
-  //const counter = useCounter();
-  return (
-    <div>
+    const { count, increaseBy } = useCounter();
+    return (
+        <div>
             <h3>
                 Counter
-								{/* 3 */}
                 <small>{" " + count}</small>
             </h3>
             <div className="flex gap-4">
-                <button
-                    className="p-2 bg-blue-500 rounded-xs w-10 text-white"
-                    // 3 
-                    onClick={() => increaseBy(+1)}
-                >
-                    +1
-                </button>
-                <button
-                    className="p-2 bg-blue-500 rounded-xs w-10 text-white"
-                    // 3
-                    onClick={() => increaseBy(-1)}
-                >
-                    -1
-                </button>
+                {/* 2. */}
+                <StyledButton
+                    label={"+1"}
+                    color="bg-red-500"
+                    handleClick={() => increaseBy(1)}
+                />
+                <StyledButton
+                    label={"-1"}
+                    color="bg-red-500"
+                    handleClick={() => increaseBy(-1)}
+                />
             </div>
         </div>
-  )
+    )
 }
