eb127c1787f1f3f0b9afa42fed91cd54a5ca5d6e
Alix Perafan <a.perafan@unimayor.edu.co>
2026-05-20 09:38:56 -0500

    reto 2

---
+++ s14-conceptos-importantes-y-retos/reto2/src/App.tsx
@@ -0,0 +1,12 @@
+import ImageManager from "./components/ImageManager";
+
+function App() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "Arial, sans-serif" }}>
+      <h1>🗂️ Administrador de Imágenes</h1>
+      <ImageManager />
+    </div>
+  );
+}
+
+export default App;
