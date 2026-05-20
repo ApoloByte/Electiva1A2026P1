dc2259c7e4e76ab287e20f920d46f58c645e6b9a
Eider Ceron <eidercsinterseminario@gmail.com>
2026-05-20 08:51:06 -0500

    feat:

--- s14-conceptos-importantes-y-retos/react-modular-lab/src/App.tsx
+++ s14-conceptos-importantes-y-retos/react-modular-lab/src/App.tsx
@@ -7,13 +7,10 @@ import { Counter } from './components/Counter'
 
 function App() {
 
-
   return (
-
 	<div className="flex flex-col items-center h-svh w-svw mt-6 gap-2 justify-center">
 		<Counter />
 	</div>
-
   )
 }
 
