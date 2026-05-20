994c4109b817fde0f2193565c09f99c064cde612
Juan Perafan <perafandiazjuanesteban@gmail.com>
2026-05-20 08:54:47 -0500

    s14

---
+++ s14-conceptos-importantes-y-retos/react-modular-lab/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Counter } from './components/Counter.tsx'
+
+function App() {
+
+  return (
+    <div className="flex flex-col items-center h-svh w-svw mt-6 gap-2 justify-center">
+      <Counter />
+    </div>
+  )
+}
+
+export default App
