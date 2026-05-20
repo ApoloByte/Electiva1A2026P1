66d67b5601f99d91c4a7018b5ebfb6650dab6dde
Alix Perafan <a.perafan@unimayor.edu.co>
2026-05-20 09:12:40 -0500

    Props

--- s14-conceptos-importantes-y-retos/react-modular-lab/src/compontents/Counter.tsx
+++ s14-conceptos-importantes-y-retos/react-modular-lab/src/compontents/Counter.tsx
@@ -1,8 +1,28 @@
 import React from "react";
-
- export const Counter = () => {
-    
+import { useCounter } from "../hooks/useCounter";
+// 1.
+import { StyledButton } from "./StyledButton";
+export const Counter = () => {
+    const { count, increaseBy } = useCounter();
     return (
-        <div>Counter</div>
+        <div>
+            <h3>
+                Counter
+                <small>{" " + count}</small>
+            </h3>
+            <div className="flex gap-4">
+                {/* 2. */}
+                <StyledButton
+                    label={"+1"}
+                    color="bg-red-500"
+                    handleClick={() => increaseBy(1)}
+                />
+                <StyledButton
+                    label={"-1"}
+                    color="bg-red-500"
+                    handleClick={() => increaseBy(-1)}
+                />
+            </div>
+        </div>
     )
-} 
+}
