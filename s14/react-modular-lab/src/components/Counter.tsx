df298f4fac0e7117728ebaec21a146d5990851e7
JohanSTivenRengifo <johanstivenrengifo@outlook.com>
2026-05-20 07:43:34 -0500

    add s14 complete activity 2

--- s14/react-modular-lab/src/components/Counter.tsx
+++ s14/react-modular-lab/src/components/Counter.tsx
@@ -1,7 +1,27 @@
-import React from "react";
-
+import { useCounter } from "../hooks/useCounter";
+// 1.
+import { StyledButton } from "./StyledButton";
 export const Counter = () => {
-    return(
-        <div>Counter</div>
+    const { count, increaseBy } = useCounter();
+    return (
+        <div>
+            <h3>
+                Counter
+                <small>{" " + count}</small>
+            </h3>
+            <div className="flex gap-4">
+                {/* 2. */}
+                <StyledButton
+                    label={"+1"}
+                    color="bg-red-500"
+                    handleClick={() => increaseBy(1)}
+                />
+                <StyledButton
+                    label={"-1"}
+                    color="bg-red-500"
+                    handleClick={() => increaseBy(-1)}
+                />
+            </div>
+        </div>
     )
 }
