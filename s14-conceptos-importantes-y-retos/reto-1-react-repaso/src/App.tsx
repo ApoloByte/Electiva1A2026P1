787009ffd0205f1686fadc86964f3b455f4b89a1
Juan Garcia <juanes19gh@gmail.com>
2026-05-20 09:36:23 -0500

    Interfaz animal reto 1 sm14

--- s14-conceptos-importantes-y-retos/reto-1-react-repaso/src/App.tsx
+++ s14-conceptos-importantes-y-retos/reto-1-react-repaso/src/App.tsx
@@ -1,9 +1,26 @@
+// 1. Importamos el componente y la interfaz usando sus rutas
 import Bienvenida from './components/Bienvenida';
+import { type Animal } from './interfaces/Animal';
 
 function App() {
+  // 2. Aquí podemos usar la interfaz para crear un objeto de tipo Animal
+  const miAnimal: Animal = {
+    name: "Toro",
+    age: 3,
+    color: "Cafe",
+    isPet: true,
+    height: 0.5
+  };
+
   return (
     <div>
+      {/* 3. Aquí mostramos el componente de bienvenida */}
       <Bienvenida />
+
+      {/* Solo para ver que los datos del animal funcionan */}
+      <div style={{ textAlign: 'center', marginTop: '20px' }}>
+        <p>Animal : {miAnimal.name} ({miAnimal.color})</p>
+      </div>
     </div>
   );
 }
